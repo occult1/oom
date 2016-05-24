8d48d70fc75742a6606edf8a68356b6a6d2aadbb
occult1 <occult@hotmail.de>
2016-05-24 15:51:41 +0200

    add project Task2

---
+++ tasks/Task2/Task2/Film.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    class Film
+    {
+        private int erscheinungsJahr;
+        private string titel;
+
+        public Film(int jahr, string title)
+        {
+            Jahr = jahr;
+            UpdateTitle(title);
+        }
+
+        public int Jahr
+        {
+            get
+            {
+                return erscheinungsJahr;
+            }
+            set
+            {
+                if (value < 0) throw new Exception("Upps");
+                erscheinungsJahr = value;
+            }
+        }
+
+        public string Titel
+        {
+            get
+            {
+                return titel;
+            }            
+        }
+
+        public void UpdateTitle(string title)
+        {
+            if (string.IsNullOrWhiteSpace(title)) throw new Exception("Titel darf nicht leer sein");
+            this.titel = title;
+        }
+
+    }
+}
---
+++ tasks/Task2/Task2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                var a = new Film(1999, "Matrix");
+                Console.WriteLine(a.Titel);
+                Console.WriteLine(a.Jahr);
+                a.UpdateTitle("The Sixth Sense");
+                Console.WriteLine(a.Titel);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error:{ex}");
+            }
+        }
+    }
+}
